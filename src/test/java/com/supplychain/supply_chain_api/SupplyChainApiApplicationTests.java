9806983d04046ec481ecb40242aa1a4b74e610ce
GABRIEL ALBUQUERQUE ALENCAR <gabriel.alencar@GASPARVIANNA.PA.GOV.BR>
2026-01-26 11:20:43 -0300

    init: estrutura inicial e gitignore

---
+++ src/test/java/com/supplychain/supply_chain_api/SupplyChainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplychain.supply_chain_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
