88b976bf171455cf005932c6b17c9facd015cc1b
Gaalbu <gabrielalbuquerquealencar@gmail.com>
2026-01-27 17:07:47 -0300

    feat: logica de nomeacao alterada

--- src/main/java/com/supplychain/supply_chain_api/domain/entity/Pacote.java
+++ src/main/java/com/supplychain/supply_chain_api/domain/entity/Pacote.java
@@ -1,6 +1,8 @@
 package com.supplychain.supply_chain_api.domain.entity;
 
+// IMPORTANTE: Importar o novo Enum com nome em português
 import com.supplychain.supply_chain_api.domain.enums.EntregaStatus;
+
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -8,32 +10,32 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "tb_packages")
+@Table(name = "tb_pacotes") 
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Pacote {
+public class Pacote { 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String trackingCode;
+    private String codigoRastreio; 
 
     @Column(nullable = false)
-    private String recipientName;
+    private String destinatario; 
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private EntregaStatus status;
+    private EntregaStatus status; 
 
     @CreationTimestamp
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    @Column(name = "data_criacao", nullable = false, updatable = false)
+    private LocalDateTime dataCriacao; 
 
     @UpdateTimestamp
-    @Column(name = "updated_at")
-    private LocalDateTime lastUpdate;
+    @Column(name = "ultima_atualizacao")
+    private LocalDateTime ultimaAtualizacao; 
 }
--- src/main/java/com/supplychain/supply_chain_api/domain/enums/EntregaStatus.java
+++ src/main/java/com/supplychain/supply_chain_api/domain/enums/EntregaStatus.java
@@ -1,9 +1,9 @@
 package com.supplychain.supply_chain_api.domain.enums;
 
 public enum EntregaStatus {
-    PREPARING_FOR_SHIPMENT,
-    IN_TRANSIT,
-    OUT_FOR_DELIVERY,
-    DELIVERED,
-    RETURNED
+    PREPARACAO,    
+    EM_TRANSITO,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE,
+    DEVOLVIDO
 }
